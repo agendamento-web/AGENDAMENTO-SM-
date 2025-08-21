8057e810bba04a3e36998aa85b710ffaadd54ae0
agendamento-web <agendamento@simonetti.com.br>
2025-08-21 15:42:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "simonetti-v" + new Date().getTime();
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./agendamento.html",
+  "./recebimento.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./favicon.ico"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.hostname.includes("google") || url.hostname.includes("cdn")) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
